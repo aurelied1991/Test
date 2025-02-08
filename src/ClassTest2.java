86e7bf0bf130ffc3618f1b9cf3d212fc53f4ff20
aureliedem <aurelie.demange1991@gmail.com>
2025-02-08 12:01:34 +0100

    Modificiation du message en "Hello World"

--- src/ClassTest2.java
+++ src/ClassTest2.java
@@ -2,7 +2,7 @@
 public class ClassTest2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello World");
 		// TODO Auto-generated method stub
 
 	}
